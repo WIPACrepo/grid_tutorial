426a6ab613dd8b8cd49c61a6535dcda365be84cf
Heath Skarlupka <skarlupka@wisc.edu>
2017-09-18 14:25:55 -0500

    Adding first job

---
+++ first_job/job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+printf "Start time: "; /bin/date
+printf "Job is running on node: "; /bin/hostname
+printf "Job running as user: "; /usr/bin/id
+printf "Job is running in directory: "; /bin/pwd
+echo "Working hard..."
+sleep $1
+echo "Job complete!"
