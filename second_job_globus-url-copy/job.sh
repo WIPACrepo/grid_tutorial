d821df0ab93c4db915c7c6be381ecc21f1122c1c
Heath Skarlupka <skarlupka@wisc.edu>
2017-09-19 09:32:51 -0500

    Adding glbous-url-copy example

---
+++ second_job_globus-url-copy/job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INPUT_DIR=$1
+FILE_NAME=$2
+OUTPUT_DIR=$3
+
+eval `/cvmfs/icecube.opensciencegrid.org/py2-v2/setup.sh`
+
+globus-url-copy gsiftp://gridftp.icecube.wisc.edu/${INPUT_DIR}/${FILE_NAME} ./
+
+##############################
+######## Do Work Here ########
+/usr/bin/md5sum ${FILE_NAME} > ${FILE_NAME}.md5
+##############################
+
+globus-url-copy ./${FILE_NAME}.md5 gsiftp://gridftp.icecube.wisc.edu/${OUTPUT_DIR}/${FILE_NAME}.md5
